f7698d50da6fae039b714339bd011e0629164761
marquezin <gabrielmarques2003@gmail.com>
2024-08-07 17:44:14 -0300

    terminando componentes e aplicando responsividade

--- Fundamentos/src/Components/Sidebar.jsx
+++ Fundamentos/src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return(
@@ -8,7 +9,7 @@ export function Sidebar(){
              className={styles.cover}
              src="https://images.unsplash.com/photo-1605379399642-870262d3d051?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZGV2ZWxvcGVyfGVufDB8fDB8fHww" />
             <div className= {styles.profile}>
-                <img src="https://github.com/marquezzin.png" />
+                <Avatar src="https://github.com/marquezzin.png" /> 
                 <strong>Gabriel Marques</strong>
                 <span>Web Developer</span>
             </div>
