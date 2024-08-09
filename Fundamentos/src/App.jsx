5a18e7c84624160214e7a1aaffd1443e0e2af76e
marquezin <gabrielmarques2003@gmail.com>
2024-08-09 19:23:28 -0300

    useState para comment

--- Fundamentos/src/App.jsx
+++ Fundamentos/src/App.jsx
@@ -3,22 +3,58 @@ import {Post} from './Components/Post'
 import styles from './App.module.css'
 import './global.css' //ja aplica a estilização
 import { Sidebar } from './Components/Sidebar'
-function App() {
+
+//author: {avatar_url:"", name:"" , role: ""}
+//publishedAt : Date
+//content : String
+const posts = [
+   {
+      id:1,
+      author:{
+         avatarUrl:"https://github.com/marquezzin.png",
+         name:"Gabriel Marques" ,
+         role: "Developer"
+      },
+      content: [
+         {type: 'paragraph', content :"Fala galera, acabei de terminar meu primeiro projeto em React!"},
+         {type: 'link', content:"caetano.design/doctorcare"}
+      ],
+      publishedAt: new Date("2024-08-07 20:00:00")
+   },
+   {
+      id:2,
+      author:{
+         avatarUrl:"https://github.com/caefleury.png",
+         name:"Caetano Korilo" ,
+         role: " Java Developer"
+      },
+      content: [
+         {type: 'paragraph', content :"Fala galera, acabei de terminar meu primeiro projeto em React!"},
+         {type: 'link', content:"caetano.design/doctorcare"}
+      ],
+      publishedAt: new Date("2024-08-06 20:00:00")
+   }
+]
+
+ export function App() {
 
   return (
    <div>
       <Header />
+
       <div className={styles.wrapper}>
          <Sidebar />
          <main>
-         <Post
-         author = "Gabriel Marques"
-         content = "conteudo"
-         />
-         <Post
-            author = "Giulia Araujo"
-            content = "conteudo 2"
-         />
+            {posts.map(post => {
+                return( 
+                <Post 
+                  author = {post.author}
+                  content = {post.content}
+                  publishedAt = {post.publishedAt}
+                />
+            
+               )
+            })}
          </main>
       </div>
    </div>
--- Fundamentos/src/Components/Post.jsx
+++ Fundamentos/src/Components/Post.jsx
@@ -1,46 +1,82 @@
-import { Avatar } from './Avatar';
-import { Comment } from './Comment';
-import styles from './Post.module.css';
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
+import { useState } from "react";
 
-export function Post(){
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src="https://github.com/caefleury.png"/>
-                    <div className={styles.authorInfo}>
-                        <strong>Caetano Korilo</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
 
-                <time  title='11 de maio às 13:05' dateTime='2022-05-11'>Publicado há 1h</time>
-            </header>
+export function Post({ author, publishedAt , content}) {
+//funcionalidade dos comentarios funcionando para todo post
+const [comments,setComments] = useState([ //valor inicial
+  1,
+  2
+])
 
-            <div className={styles.content}>
-                <p>Fala galera, acabei de terminar meu primeiro projeto em React!</p>
-                <p> <a href="">caetano.design/doctorcare</a></p>
-                <p> <a href="">#meuprimeiroprojeto</a> {'  '}
-                    {/* espaçamento rápido */}
-                    <a href="">#rocketseat</a>
-                </p>
-            </div>
-            <form className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+  //uso da 'date-fns'
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
-                <textarea
-                 placeholder='Deixe um comentário'
-                 />
-            <footer>
-                <button type="submit">Publicar</button>
-            </footer>
-            </form>
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    //prefixo
+    addSuffix: true,
+  });
 
-            <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
-            </div>
-        </article>
-    )
+  
+
+  function handleCreateNewComment() {
+    event.preventDefault() //nao ter redirecionamento
+    
+    setComments([...comments,comments.length +1]) //passa o novo valor, não apenas oq quer inserir
+    
+  }
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+            {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+            if (line.type == "paragraph"){
+                return <p>{line.content}</p>
+            } else if (line.content){
+                return <p><a href="">{line.content}</a></p>
+            }
+        })}
+      </div>
+      <form onSubmit={handleCreateNewComment} //incrementa a lista comments
+       className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment =>{ //expõe os comentarios de acordo com os elementos da lista
+          return <Comment/>
+        })}
+      </div>
+    </article>
+  );
 }
